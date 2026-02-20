7ad911d133bff058010d0b9b8c0832be0db54c79
StevenRice99 <Contact@StevenRice.ca>
2026-02-20 14:32:40 -0500

    start of python helpers

---
+++ Install.bat
@@ -0,0 +1,5 @@
+uv venv --python 3.10.12
+.venv\Scripts\activate
+uv pip install torch~=2.2.1 --index-url https://download.pytorch.org/whl/cu121
+uv pip install mlagents==1.1.0
+uv pip install "setuptools<70.0.0"
